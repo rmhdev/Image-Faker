8767828685456cc50da419d9524d52c825f2854f
Rober Martín H <rmh.dev@gmail.com>
2013-03-31 18:38:25 +0200

    Adding monolog.

--- src/development.php
+++ src/development.php
@@ -1,7 +1,10 @@
 <?php
-
+/* @var $app \Silex\Application */
 $app = require_once __DIR__.'/production.php';
 
 $app["debug"] = true;
+$app->register( new Silex\Provider\MonologServiceProvider(), array(
+    "monolog.logfile" => __DIR__ . "/../logs/development.log"
+));
 
 return $app;
