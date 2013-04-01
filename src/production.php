d3db05172de85b77abeb18b0fdae52305931d1fb
Rober Martín H <rmh.dev@gmail.com>
2013-04-01 13:15:34 +0200

    Adding twig for the homepage.

--- src/production.php
+++ src/production.php
@@ -2,8 +2,13 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+/* @var $app \Silex\Application */
 $app = require __DIR__.'/app.php';
-//config here
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__ . "/../templates/"
+));
+
 require __DIR__.'/controllers.php';
 
 return $app;
