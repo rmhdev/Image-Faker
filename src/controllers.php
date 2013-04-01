af4f05267fdfc1be075080b23372c024557e3ae3
Rober Martín H <rmh.dev@gmail.com>
2013-04-01 17:21:52 +0200

    Basic use of twig

--- src/controllers.php
+++ src/controllers.php
@@ -4,8 +4,12 @@ use Symfony\Component\HttpFoundation\Response;
 use ImageFaker\Image\ImageConfig;
 use ImageFaker\Image\Image;
 
-$app->get("/", function () {
-    return "Hello world!";
+$app->get("/", function () use ($app) {
+    return new Response(
+        $app['twig']->render("homepage.twig", array()),
+        200,
+        array()
+    );
 });
 
 $app->get("/{background}/{size}.{extension}", function ($background, $size, $extension) use ($app) {
